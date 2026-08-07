2c9cc1f9318e05b0390f0d66f95a893b85d59e02
irin <rhkrtlsd2486@naver.com>
2026-08-07 15:16:50 +0900

    Add ReNamer development SDK package boundary

---
+++ tools/sdk-packages/renamer_sdk/__init__.py
@@ -0,0 +1,17 @@
+"""Development SDK for the ReNamer project.
+
+This package is repository tooling. Product runtime code must not depend on it.
+"""
+
+__all__ = [
+    "core_sdk",
+    "build_sdk",
+    "test_sdk",
+    "sdk_suite",
+    "observability_sdk",
+    "validation_sdk",
+    "model_sdk",
+    "integration_sdk",
+    "migration_sdk",
+    "domain_sdk",
+]
